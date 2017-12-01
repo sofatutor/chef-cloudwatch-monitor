e98674b137fd4225dcb3bac625e0b0f7003a8fb2
Kornelius Kalnbach <murphy@rubychan.de>
2017-12-01 14:49:21 +0100

    "MAILTO=" is a syntax error. Just remove it?

--- recipes/config.rb
+++ recipes/config.rb
@@ -11,9 +11,8 @@ options = ['--from-cron'] + node['cwmon']['options']
 
 # Configure cron job
 cron_d 'cloudwatch-monitor' do
-  minute node['cwmon']['interval'] == 1 ? '*' : "*/#{node['cwmon']['interval']}"
+  minute "*/#{node['cwmon']['interval']}"
   user node['cwmon']['user']
-  mailto ''
   path '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
   shell '/bin/bash'
   command %Q(#{cwmon_home_link}/mon-put-instance-data.pl #{(options).join(' ')})
